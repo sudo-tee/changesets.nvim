ce64d2bb64d6c5d77cdf65d9abd98aefc91b2991
Francis Belanger <francis.belanger@gmail.com>
2024-11-08 07:35:17 -0500

    feat: add get_default_text option for changesets
    Adds a new option `get_default_text` to allow setting default text
    for new changesets. This function can be used to pre-fill new
    changesets with default content.

--- lua/changesets/opts.lua
+++ lua/changesets/opts.lua
@@ -6,19 +6,35 @@ local M = {}
 ---@type Release[]
 M.RELEASE_KINDS = { 'patch', 'minor', 'major' }
 
+---@class changesets.Opts
+---@field cwd string
+---@field changeset_dir string
+---@field changed_packages_marker string
+---@field changed_packages_highlight string
+---@field get_default_text fun(): string
+
+---@type changesets.Opts
 M.default_opts = {
   cwd = vim.fn.getcwd(),
   changeset_dir = '.changesets',
   changed_packages_marker = '~',
   changed_packages_highlight = 'TelescopeResultsConstant',
+  get_default_text = function()
+    return ''
+  end,
 }
 
+---@type changesets.Opts
 local _opts = vim.tbl_deep_extend('force', {}, M.default_opts)
 
+---Setup the options for the plugin
+---@param opts changesets.Opts
 M.setup = function(opts)
-  _opts = vim.tbl_deep_extend('force', M.default_opts, opts)
+  _opts = vim.tbl_deep_extend('force', M.default_opts, opts or {})
 end
 
+---Get the current options for the plugin
+---@return changesets.Opts
 M.opts = function()
   return _opts
 end
